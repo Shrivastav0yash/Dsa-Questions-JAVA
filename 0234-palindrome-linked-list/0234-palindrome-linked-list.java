e14a0395c520c56854db0cda9d92ebc2cbdb26c1
Yash Shrivastav <110709758+Shrivastav0yash@users.noreply.github.com>
2026-08-10 12:47:19 +0530

    Time: 14 ms (9.99%), Space: 102.1 MB (5.32%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,36 +10,23 @@
  */
 class Solution {
 
+    ListNode curr ;
+
+    public boolean recur(ListNode head){
+
+        if(head == null) return true;
+
+        boolean ans = recur(head.next);
+
+        if(head.val != curr.val) return false;
+
+        curr = curr.next;
+
+        return ans;
+    }
+
     public boolean isPalindrome(ListNode head) {
-
-        if(head == null || head.next == null) return true;
-        
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
-
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            ListNode temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
-        }
-
-        //Odd length case
-        if(fast != null){
-            slow = slow.next;
-        }
-
-        while(prev != null && slow != null){
-            if(prev.val != slow.val){
-                return false;
-            }
-            prev = prev.next;
-            slow = slow.next;
-        }
-
-        return true;
-
+        curr = head;
+        return recur(head);
     }
 }
