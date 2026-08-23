8c86d9b8beb863c91624a6c828d1c0084513721c
Yash Shrivastav <110709758+Shrivastav0yash@users.noreply.github.com>
2026-08-23 19:16:27 +0530

    Time: 2 ms (90.43%), Space: 43 MB (91.7%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,14 +1,20 @@
 class Solution {
 
-    public void bubbleSort(int[] arr){
-        for(int i=0; i<arr.length - 1; i++){
-            for(int j=0; j<arr.length - i - 1; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+    public void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements greater than key one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+
+            // Insert key at its correct position
+            arr[j + 1] = key;
         }
     }
 
@@ -20,7 +26,7 @@ class Solution {
             arr[i] = heights[i];
         }
 
-        bubbleSort(arr);
+        insertionSort(arr);
 
         int count = 0;
 
