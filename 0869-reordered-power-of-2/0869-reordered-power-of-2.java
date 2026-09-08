97db5556a9a3087711fa5f5e11036813a9912dd0
Yash Shrivastav <110709758+Shrivastav0yash@users.noreply.github.com>
2026-09-08 11:11:18 +0530

    Time: 5 ms (6.43%), Space: 42.9 MB (14.04%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.java
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.java
@@ -1,23 +1,30 @@
 class Solution {
 
-    public String getSortedStr(int num) {
+    HashSet<String> set = new HashSet<>();
 
-        String str = String.valueOf(num);
-        char[] arr = str.toCharArray();
-        Arrays.sort(arr);
-        String result = new String(arr);
-        return result;
+    public void buildSet() {
+
+        for (int p = 0; p <= 29; p++) {
+            String str = String.valueOf(1 << p);
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String result = new String(arr);
+            set.add(result);
+        }
     }
 
     public boolean reorderedPowerOf2(int n) {
 
-        String s = getSortedStr(n);
-
-        for(int p=0; p<= 29; p++){
-            if(s.equals(getSortedStr(1 << p)))
-                return true;
+        if (set.isEmpty()) {
+            buildSet();
         }
 
-        return false;
+        String str = String.valueOf(n);
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        String result = new String(arr);
+
+        return set.contains(result);
+
     }
 }
